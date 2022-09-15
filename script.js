d349f91376394625b0b673060c5a96fb8cd12eff
Megan Rakow <meganrakow@Megans-MBP.lan>
2022-09-14 21:01:07 -0500

    edited java

--- script.js
+++ script.js
@@ -13,9 +13,18 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 function generatePassword(){
-  var length = prompt('Password length is atleast 8 characters and no more than 128 characters');
+  var password = prompt('Password length is atleast 8 characters and no more than 128 characters');
+  if (password.length < 8){
+    alert("please enter new password");
+     password = prompt('Password length is atleast 8 characters and no more than 128 characters');
+  }else{
+    password = parseInt(password)
+    console.log(typeof password)
+  }
+  var characters = prompt('Include lowercase, uppercase, numeric, and/or special characters');
+  if (characters){
+    
+  }
   
-  length = parseInt(length)
-  console.log(typeof length)
 }
 
