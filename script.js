30ed3c00ba9fbae8c7470801561e9aeaf3537de3
Megan Rakow <meganrakow@Megans-MBP.lan>
2022-09-16 19:40:52 -0500

    edited java

--- script.js
+++ script.js
@@ -16,21 +16,45 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 function generatePassword(){
-  var password = prompt('Password length is atleast 8 characters and no more than 128 characters');
+  var password = prompt('Please Enter # thats password length is atleast 8 characters and no more than 128 characters');
   password = parseInt(password)
   console.log( password)
   
   if (password < 8 || password > 128){
     alert("please enter a number between 8 and 128");
-     
+     generatePassword()  
+  }else{ 
     
-  }else{
-    var characters = confirm('Should it include lowercase');
-    if (true){
-
-    }
+    /* password.length =  */
   }
-  
-  
+  var lowerCharacters = confirm('Should it include lowercase');
+    if (true){
+      lowerCharacters = lowerCase.split("")
+    } else{
+      
+    }
+
+  var upperCharacters = confirm('Should it include uppercase');
+    if (true){
+      upperCharacters = upperCase.split("");
+    } else{
+      
+    }
+
+  var numbers = confirm('Should it include numbers');
+    if (true){
+      numbers = numeric.split("");
+    } else{
+      
+    }
+
+  var special = confirm('Should it include special characters');
+    if (true){
+      special = specialChar.split("");
+    } else{
+      
+    }
+
+    var passwordArray = [special, numbers, upperCharacters, lowerCharacters]
 }
 
