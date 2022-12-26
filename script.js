69223c232fec3f43dd4b4afc91ac45ded87062bf
Megan Rakow <meganrakow@Megans-MacBook-Pro-2.local>
2022-12-26 09:45:52 -0600

    fixes up to project

--- script.js
+++ script.js
@@ -1,63 +1,138 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var lowerCase= "abcdefghijklmnopqrstuvwxyz";
-var upperCase= "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var numeric = '0123456789';
-var specialChar = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+var lowerCase = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+var upperCase = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+var numeric = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var specialChar = [
+  "!",
+  "#",
+  "$",
+  "%",
+  "&",
+  "'",
+  '"',
+  "(",
+  ")",
+  "*",
+  "+",
+  ",",
+  "-",
+  ".",
+  "/",
+  ":",
+  ";",
+  "<",
+  ">",
+  "=",
+  "?",
+  "@",
+  "[",
+  "]",
+  "^",
+  "_",
+  "{",
+  "}",
+  "|",
+  "~",
+  "`",
+];
 
-
-
-function generatePassword(){
-  var password = prompt('Please Enter # thats password length is atleast 8 characters and no more than 128 characters');
-  password = parseInt(password)
-  
-  if (password < 8 || password > 128 || isNaN(password)){
-    alert("please enter a number between 8 and 128");
-     generatePassword()  
-  }else{ 
-    
+function generatePassword() {
+  var password = Number(
+    prompt(
+      "Please Enter # thats password length is atleast 8 characters and no more than 128 characters"
+    )
+  );
+  console.log(password);
+  while (password < 8 || password > 128 || isNaN(password)) {
+    if (password < 8 || password > 128 || isNaN(password)) {
+      alert("please enter a number between 8 and 128");
+      password = Number(
+        prompt(
+          "Please Enter # thats password length is atleast 8 characters and no more than 128 characters"
+        )
+      );
+    }
   }
 
-  var lowerCharacters = confirm('Should it include lowercase');
-    if (lowerCharacters == true){
-      lowerCharacters = lowerCase.split("")
-    } else{
-      lowerCharacters = ""
-      
-    }
+  var lowerCharacters = confirm("Should it include lowercase");
+  var upperCharacters = confirm("Should it include uppercase");
+  var numbers = confirm("Should it include numbers");
+  var special = confirm("Should it include special characters");
 
-  var upperCharacters = confirm('Should it include uppercase');
-    if (upperCharacters == true){
-      upperCharacters = upperCase.split("");
-    } else{
-      upperCharacters = ""
-    }
+  var newPassword = "";
 
-  var numbers = confirm('Should it include numbers');
-    if (numbers == true){
-      numbers = numeric.split("");
-    } else{
-      numbers = ""    
-    }
+  var chooses = [].concat(lowerCase);
+  if (lowerCharacters) chooses = chooses.concat(lowerCase);
+  if (upperCharacters) chooses = chooses.concat(upperCase);
+  if (numbers) chooses = chooses.concat(numeric);
+  if (special) chooses = chooses.concat(specialChar);
 
-  var special = confirm('Should it include special characters');
-    if (special == true){
-      special = specialChar.split("");
-    } else{
-      special = ""
-    }
-
-    var passwordArray = [...special, ...numbers, ...upperCharacters, ...lowerCharacters];
-    
-    var newPassword = "";
-
-    for (var i = 1; i <= password; i++){
-      newPassword += passwordArray[Math.floor(Math.random() * passwordArray.length)];
-    }
-    return newPassword;      
+  for (var i = 1; i <= password; i++) {
+    var randomChar = chooses[Math.floor(Math.random() * chooses.length)];
+    newPassword += randomChar;
+  }
+  return newPassword;
 }
 
-      // Write password to the #password input
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
